9a563d896b399116caf07945c7da32c1c16f9146
gyf <1061775035@qq.com>
2019-07-09 17:51:57 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-// 职责：导入项目依赖的资源（包， css ...）
-// 职责2：初始化根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+// 职责：导入项目依赖的资源（包， css ...）
+// 职责2：初始化根实例
